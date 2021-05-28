79f76014774f315fd245b9c547780c5559a49765
TheBoWT <44741673+basmed@users.noreply.github.com>
2021-05-28 03:00:15 -0400

    change nav bg color

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+
+  firebase: {
+    apiKey: "AIzaSyCX3s7dDfgI7kfgLv-OhzMzadJw99TV7nY",
+    authDomain: "cart-d129d.firebaseapp.com",
+    projectId: "cart-d129d",
+    storageBucket: "cart-d129d.appspot.com",
+    messagingSenderId: "765693924099",
+    appId: "1:765693924099:web:2b71ba6e19022b184e4f36",
+    measurementId: "G-61QRN1L07E"
+  }
 };
